657afabaebc4cc0f4851f2d9d523bdb67fb38b73
mabidux <70872559+mabidux@users.noreply.github.com>
2023-01-03 18:00:55 -0400

    Add files via upload

--- src/dev/abidux/easyql/util/serializer/Serializers.java
+++ src/dev/abidux/easyql/util/serializer/Serializers.java
@@ -9,6 +9,7 @@ public class Serializers {
     public static final Serializer<String, String> STRING_SERIALIZER = new Serializer<>("text", s -> s, s -> s);
     public static final Serializer<Integer, Integer> INTEGER_SERIALIZER = new Serializer<>("int", i -> i, i -> i);
     public static final Serializer<Float, Float> FLOAT_SERIALIZER = new Serializer<>("float", f -> f, f -> f);
+    public static final Serializer<Boolean, Boolean> BOOLEAN_SERIALIZER = new Serializer<>("boolean", b -> b, b -> b);
     public static final Serializer<ArrayList<String>, String> STRING_ARRAYLIST_SERIALIZER = new Serializer<>("text", list -> list.stream().collect(Collectors.joining("/sep/")), Serializers::decodeStringArrayList);
     public static final Serializer<ArrayList<Integer>, String> INTEGER_ARRAYLIST_SERIALIZER = new Serializer<>("text", list -> list.stream().map(String::valueOf).collect(Collectors.joining(";")), Serializers::decodeIntegerArrayList);
 
